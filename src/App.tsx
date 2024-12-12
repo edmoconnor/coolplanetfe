1dd2849b86779e2c713fac5ba179992430730f67
Ed O'Connor <edmoconnor@gmail.com>
2024-12-12 22:32:27 +0000

    getUsers hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,81 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import { getUser, getUsers } from './hooks/GetUsers.tsx';
+// import UsersList from './components/usersList.tsx';
+import { Link } from 'react-router-dom';
+
+
+import Button from '@mui/material/Button';
+// import DataTable from 'react-data-table-component';
+
 
 function App() {
+  // const [spinner, setSpinner] = useState(false);
+  const [users, setUsers] = useState([]);
   const [user, setUser] = useState([]);
+  // const [list, setList] = useState([]);
+  
   useEffect(() => {
-    fetch("http://localhost:3000/users", {
-
-    })
-    .then((response) => response.json())
-      .then((data) => {
-        setUser(data);
+    // setSpinner(true);
+    // let mounted = true;
+    getUsers()
+      .then(items => {
+        // if(mounted) {
+          setUsers(items)
+        // }
       })
-      .catch((error) => console.log(error));
   }, []);
-
+  
   return (
     <div className="App">
       <header className="App-header">
-
-      
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-
+        Coolplanet
       </header>
+
       <div>
         <h2>users</h2>
-        {user.map((data) => {
-          return(
-            <li key={data.id}> {data.first_name} {data.last_name}</li>
-          )
-        })
-        }
-              
+        {
+          users.map((data) => {
+            return(
+              <ul>
+                <Link to={'./users/' + data['id']}>
+                  <Button onClick={() => {
+                    getUser(data['id'])
+                      .then(items => {
+                      // if(mounted) {
+                        setUser(data)
+                        console.log(data)
+                      // }
+                    })
+                    console.log([data['id']])
+                  }}
+                > 
+                
+                <li key={data['id']}> {data['first_name'] } {data['last_name']}</li>
+                  </Button>
+                </Link>
+                  <>
+                    {/* {usersList} */}
+                    {/* <p>{data['first_name']}</p>
+                    <p>{data.last_name}</p>
+                    <p>{data.email}</p>
+                    <p>{data['company'].name}</p>
+                    <p>{data.company.name}</p> 
+                    <p>{data.dob}</p>
+                    <img src={data.avatar} alt=""/>  */}
+                  </>
+              </ul>
+            )
+          })
+        }    
       </div>
     </div>
   );
 }
+
+// function handleSaveClick() {
+//   console.log('✅ Progress saved');
+// }
 
 export default App;
